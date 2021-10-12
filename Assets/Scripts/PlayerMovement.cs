8e74f153fcf841bd4e459f61a95838df0574016e
fyfrnffe <ivan03072001@gmail.com>
2021-10-12 20:36:19 +0300

    0.2
    -добавил поворот персонажа в сторону движения
    -починил слои

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     private AudioSource audioSrc;
-
+	private bool facingRight = true;
     [SerializeField]
     private float speedMultiplier;
+	public SpriteRenderer sprite;
 
     void Awake()
     {
@@ -24,6 +25,26 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+		if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A))
+        {
+			if(facingRight)
+			{
+				sprite.flipX = true;
+				facingRight = false;
+
+			}
+
+		}
+		if(Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D))
+        {
+			if(!facingRight)
+			{
+				sprite.flipX = false;
+				facingRight = true;
+
+			}
+
+		}
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
             if (!audioSrc.isPlaying)
@@ -32,4 +53,13 @@ public class PlayerMovement : MonoBehaviour
             }
         }
     }
+	
+	private void Flip()
+	{
+		facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+		
+	}
 }
