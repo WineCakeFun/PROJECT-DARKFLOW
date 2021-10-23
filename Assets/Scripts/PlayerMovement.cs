b9bb776ed309fb5ec46f19840fe5209d7d1a6717
Milan Hordiienko <45905594+WineCakeFun@users.noreply.github.com>
2021-10-23 22:03:50 +0300

    ANIMATION UPDATE
    + Добавил анимацию PlayerIdle по спрайтам PlayerIdle и PlayerIdleNotBlinking.
    + Добавил анимацию бега PlayerRunSide
    + Добавил код поворота.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     private AudioSource audioSrc;
-
+    private bool facingRight = true;
     [SerializeField]
     private float speedMultiplier;
+    public SpriteRenderer sprite;
+    private Animator anim;
 
     void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
         audioSrc = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();  
     }
 
     void FixedUpdate()
@@ -24,12 +27,43 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A))
+        {
+			if(facingRight)
+			{
+				sprite.flipX = true;
+				facingRight = false;
+
+			}
+
+		}
+		if(Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D))
+        {
+			if(!facingRight)
+			{
+				sprite.flipX = false;
+				facingRight = true;
+
+			}
+
+		}
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
+            anim.SetBool("isRunning", true);
             if (!audioSrc.isPlaying)
             {
                 audioSrc.Play();
             }
+        }else{
+            anim.SetBool("isRunning", false);
         }
     }
+    private void Flip()
+	{
+		facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+		
+	}
 }
